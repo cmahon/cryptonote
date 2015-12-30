46899046aac42065b232f01378a15cdbad9da804
Chris Mahon <cnmahon@gmail.com>
2015-12-31 00:02:05 +1100

    Experimenting with seed node addresses

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "10.1.1.3:17901"
+  //"10.1.1.3:17901",
+  "172.16.65.130:17901",
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
